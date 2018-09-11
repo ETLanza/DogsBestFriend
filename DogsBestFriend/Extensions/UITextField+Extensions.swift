05376c53c89a41a43c8e73c8e074a7c82a7b5eef
Nathan Lanza <nathan@lanza.io>
2018-09-11 14:41:20 -0700

    Clean up UITextField extension

--- DogsBestFriend/Extensions/UITextField+Extensions.swift
+++ DogsBestFriend/Extensions/UITextField+Extensions.swift
@@ -10,35 +10,33 @@ import UIKit
 
 extension UITextField {
     
-    @IBInspectable var doneAccessory: Bool{
-        get{
+    @IBInspectable var doneAccessory: Bool {
+        get {
             return self.doneAccessory
         }
         set (hasDone) {
-            if hasDone{
+            if hasDone {
                 addDoneButtonOnKeyboard()
             }
         }
     }
     
-    func addDoneButtonOnKeyboard()
-    {
-        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
         doneToolbar.barStyle = .default
         
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        let done = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
         
         let items = [flexSpace, done]
         doneToolbar.items = items
         doneToolbar.sizeToFit()
         
-        self.inputAccessoryView = doneToolbar
+        inputAccessoryView = doneToolbar
     }
     
-    @objc func doneButtonAction()
-    {
-        self.resignFirstResponder()
+    @objc func doneButtonAction() {
+        resignFirstResponder()
     }
 }
 
