0148b0882f064b59fe6eff150f07c46d9db0b13d
Eric Lanza <eric@lanza.io>
2018-09-12 17:50:36 -0600

    Add boilerplate detail views Add ParkDetailVC Add DogDetailVC Add WalkDetailVC

---
+++ DogsBestFriend/Controllers/View Controllers/ParkDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ParkDetailViewController.swift
+//  DogsBestFriend
+//
+//  Created by Eric Lanza on 9/12/18.
+//  Copyright © 2018 ETLanza. All rights reserved.
+//
+
+import UIKit
+
+class ParkDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
---
+++ DogsBestFriend/Controllers/View Controllers/WalkDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WalkDetailViewController.swift
+//  DogsBestFriend
+//
+//  Created by Eric Lanza on 9/12/18.
+//  Copyright © 2018 ETLanza. All rights reserved.
+//
+
+import UIKit
+
+class WalkDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
