e73167bf77d544eb6763a50a15ffac170ef71e52
Eric Lanza <eric@lanza.io>
2018-09-13 17:23:52 -0600

    Finalize views for each viewcontroller

--- DogsBestFriend/Controllers/View Controllers/ParksViewController.swift
+++ DogsBestFriend/Controllers/View Controllers/ParksViewController.swift
@@ -12,17 +12,20 @@ import UIKit
 class ParksViewController: UIViewController {
     // MARK: - IBOutlets
 
-    @IBOutlet var zipCodeTextField: UITextField!
-    @IBOutlet var mapView: MKMapView!
-    @IBOutlet var drawerView: UIView!
-    @IBOutlet var parksTableView: UITableView!
-    @IBOutlet var drawerClosedConstraint: NSLayoutConstraint!
+    @IBOutlet weak var favoritesSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var zipCodeSearchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var drawerView: UIView!
+    @IBOutlet weak var parksTableView: UITableView!
+    @IBOutlet weak var drawerClosedConstraint: NSLayoutConstraint!
+    @IBOutlet weak var nearbyViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var favoritesViewTrailingConstraint: NSLayoutConstraint!
 
     // MARK: - Life Cycle Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        zipCodeTextField.addDoneButtonOnKeyboard()
+        zipCodeSearchBar.addDoneButtonOnKeyboard()
     }
 
     // MARK: - IBActions
@@ -36,6 +39,19 @@ class ParksViewController: UIViewController {
 
     @IBAction func drawerSwipedDown(_ sender: UISwipeGestureRecognizer) {
         drawerClosedConstraint.priority = UILayoutPriority(rawValue: 999)
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+
+    @IBAction func favoritesSegementedControlValueChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            favoritesViewTrailingConstraint.priority = UILayoutPriority(rawValue: 999)
+            nearbyViewLeadingConstraint.priority = UILayoutPriority(rawValue: 997)
+        } else if sender.selectedSegmentIndex == 1 {
+            favoritesViewTrailingConstraint.priority = UILayoutPriority(rawValue: 997)
+            nearbyViewLeadingConstraint.priority = UILayoutPriority(rawValue: 999)
+        }
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
         }
