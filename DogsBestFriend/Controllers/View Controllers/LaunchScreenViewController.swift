41b1b1e013fcca84b4f9a61e33206fe71ee809e6
Eric Lanza <eric@lanza.io>
2018-09-17 19:55:42 -0600

    Add Launch screen for requesting InUseAuth for CoreLocation

---
+++ DogsBestFriend/Controllers/View Controllers/LaunchScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchScreenViewController.swift
+//  DogsBestFriend
+//
+//  Created by Eric Lanza on 9/17/18.
+//  Copyright © 2018 ETLanza. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
