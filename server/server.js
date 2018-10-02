0dacffe8412c7d774659f70e01dc1afac834e3c1
Nathan Lanza <nathan@lanza.io>
2018-10-02 05:40:26 +0000

    Add the ability to create a user and login

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3005;
 var mongoose = require('mongoose');
+
 var Dog = require('./api/models/DogSchema');
+var user = require('./api/models/UserSchema');
+
 var bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
