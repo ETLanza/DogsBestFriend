45c240ca48a8d8118fa66d8b4961a706101194df
Nathan Lanza <nathan@lanza.io>
2018-10-07 19:06:03 -0700

    Clean up some details with mongodb

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3005;
-var mongoose = require('mongoose');
 
 var Dog = require('./api/models/DogSchema');
 var user = require('./api/models/UserSchema');
 
 var bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+const dbConfig = require('./config/database.config.js');
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/dbfdb');
+mongoose.connect(dbConfig.url, {
+  useNewUrlParser: true
+}).then(() => {
+  console.log("Sucecessfully connected to the database");
+}).catch(error => {
+  console.log('Could not connec to the database. Exiting now...', error);
+});
+
+app.get('/', (request, response) => {
+  response.json({"message": "Test"});
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
