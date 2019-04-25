50ba49a6d9a7c5cefc4aeb5b9fcfdbca528b29b9
Eric Lanza <eric@lanza.io>
2019-04-24 22:17:41 -0600

    Update walk model and keys

--- DogsBestFriend/Models/Walk.swift
+++ DogsBestFriend/Models/Walk.swift
@@ -7,35 +7,46 @@
 //
 
 import Foundation
+import Firebase
 
-class Walk: Equatable, Codable {
+class Walk: Equatable {
     
     var distance: Double
     var timestamp: Date
     var duration: Int
     var locations: [Location]
+    var documentRef: DocumentReference
+    var ownerDocumentRef: DocumentReference
+    var uuid: String
     
-    init(distance: Double, timestamp: Date, duration: Int, locations: [Location] = []) {
+    init(distance: Double, timestamp: Date, duration: Int, locations: [Location] = [], documentRef: DocumentReference, ownerDocumentRef: DocumentReference) {
         self.distance = distance
         self.timestamp = timestamp
         self.duration = duration
         self.locations = locations
+        self.documentRef = documentRef
+        self.ownerDocumentRef = documentRef
+        self.uuid = documentRef.documentID
     }
     
     static func == (lhs: Walk, rhs: Walk) -> Bool {
-        return lhs.distance == rhs.distance && lhs.timestamp == rhs.timestamp && lhs.duration == rhs.duration
+        return lhs.uuid == rhs.uuid
     }
 }
 
 extension Walk {
-    convenience init?(jsonDictionary: [String: Any]) {
-        guard let distance = jsonDictionary[Keys.Walk.distance] as? Double,
-            let timestamp = jsonDictionary[Keys.Walk.distance] as? Date,
-            let duration = jsonDictionary[Keys.Walk.duration] as? Int,
-            let locations = jsonDictionary[Keys.Walk.locations] as? [Location]
+    convenience init?(dictionary: [String: Any]) {
+        guard let distance = dictionary[Keys.Walk.distance] as? Double,
+            let timestamp = dictionary[Keys.Walk.distance] as? Date,
+            let duration = dictionary[Keys.Walk.duration] as? Int,
+            let locationsArray = dictionary[Keys.Walk.locations] as? [String:[String: Any]],
+            let documentRef = dictionary[Keys.Walk.documentRef] as? DocumentReference,
+            let ownerDocumentRef = dictionary[Keys.Walk.ownerDocumentRef] as? DocumentReference
             else { return nil }
         
-        self.init(distance: distance, timestamp: timestamp, duration: duration, locations: locations)
+        let locations: [Location] = locationsArray.compactMap( { Location(jsonDictionary: $0.value) })
+        
+        self.init(distance: distance, timestamp: timestamp, duration: duration, locations: locations, documentRef: documentRef, ownerDocumentRef: ownerDocumentRef)
     }
     
     var asDictionary: [String: Any] {
@@ -48,8 +59,8 @@ extension Walk {
                 Keys.Walk.locations: locationsAsDictionaries]
     }
     
-    var asData: Data? {
-        let encoder = JSONEncoder()
-        return try? encoder.encode(self)
-    }
+    //    var asData: Data? {
+    //        let encoder = JSONEncoder()
+    //        return try? encoder.encode(self)
+    //    }
 }
